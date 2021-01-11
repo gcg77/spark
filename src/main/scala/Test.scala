eaec15cabece9a8b66617e70fa542a8c264be75a
gcg77 <759672969@qq.com>
2021-01-11 17:22:37 +0800

    spark test

--- src/main/scala/Test.scala
+++ src/main/scala/Test.scala
@@ -1,4 +1,9 @@
 import org.apache.spark.{SparkConf, SparkContext}
 
 object Test {
-  def main(args: Array
+  def main(args: Array[String]): Unit = {
+    val conf=new SparkConf().setAppName("SparkTest")
+    new SparkContext(conf)
+    println("hello word")
+  }
+}
